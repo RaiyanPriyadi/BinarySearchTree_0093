7514b34ff95502873c71d532c538cb8f9de3cb97
RaiyanPriyadi <dwi.raiyan.ft25@mail.umy.ac.id>
2026-06-11 18:06:22 +0700

    void preorder

--- searchTree.cpp
+++ searchTree.cpp
@@ -90,5 +90,26 @@ public:
         }
 
         if (ptr != NULL)
-        
+        {
+            inorder(ptr->leftchild);
+            cout << ptr->info << " ";
+            inorder(ptr->rightchild);
+        }
    }
+   void preorder(Node *ptr)
+   {
+        if (ROOT == NULL)
+        {
+            cout << "tree is empty" << endl;
+            return;    
+        }
+
+        if (ptr != NULL)
+        {
+            cout << ptr->info << " ";
+            preorder(ptr->leftchild);
+            preorder(ptr->rightchild);
+        }
+   }
+
+   void 
