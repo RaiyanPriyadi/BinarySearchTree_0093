c118f524a7f3e7491e88b0f71535912963b763ad
RaiyanPriyadi <dwi.raiyan.ft25@mail.umy.ac.id>
2026-06-11 17:52:08 +0700

    first commit

---
+++ searchTree.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Node 
+{
+public:
+    string info;
+    Node *leftchild;
+    Node *rightchild;
+
+    // constructor for the node class
+    Node(string i, Node *1, Node *r) 
+    {
+        info = i;
+        leftchild = l;
+        rightchild = r;
+    }
+
+}
+
+class BinaryTree
+{
+public: 
+    Node *ROOT;
+
+    BinaryTree()
+    {
+        ROOT = NULL; // instalizing ROOT to NULL
+    }
+
+    //insert a node in the binary search tree
+    void insert(string element)
+    {
+        Node *newNode = new Node(element, NULL, NULL);
+
+        newNode->info = element;
+        newNode->leftchild = NULL;
+        newNode->rightchild = NULL;
+
+        Node *parent = NULL;
+        Node *currentNode = ROOT;
+
+        search(element, parent, currentNode);
+
+        if (parent == NULL)
+        {
+            ROOT = newNode;
+            return;
+        }
+
+        
+    }
